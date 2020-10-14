ede27afa0e8e4cd2b6d9f2d5f5c9599eee223672
Everton Silva <everton.silva@mv.com.br>
2020-10-14 18:24:06 -0300

    commit inicial, tela de categorias com dados mockados

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Routes from './routes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Montserrat',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif'
+    ].join(','),
+  }
+});
+
+const App: React.FC = () => (
+    <ThemeProvider theme={theme}>
+      <Routes/>
+    </ThemeProvider>
+);
 
 export default App;
